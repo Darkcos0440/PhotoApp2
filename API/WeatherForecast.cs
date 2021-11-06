ee53aff2a083b04aa6dd2342363e02b7a769f232
Abie <33075662@student.g.nwu.ac.za>
2021-11-06 22:14:49 +0200

    Test Github - New SSD

--- API/WeatherForecast.cs
+++ API/WeatherForecast.cs
@@ -11,5 +11,9 @@ namespace API
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
+
+        //test
+
+        public string Summary { get; set; }
     }
 }
